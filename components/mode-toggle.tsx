d23922fe6d905bdb56a6355b70a5bc59d02795b0
guillermouzner <guillermouz16@gmail.com>
2023-06-19 13:50:12 -0300

    carbobasic

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="h-8 w-8 px-0" size="sm" variant="ghost">
+        <Button className="h-8 w-8 px-0 hidden" size="sm" variant="ghost">
           <SunMedium className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
@@ -29,11 +29,11 @@ export function ModeToggle() {
           <SunMedium className="mr-2 h-4 w-4" />
           <span>Light</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
           <Moon className="mr-2 h-4 w-4" />
           <span>Dark</span>
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
           <Laptop className="mr-2 h-4 w-4" />
           <span>System</span>
         </DropdownMenuItem>
